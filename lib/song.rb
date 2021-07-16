2f7fd9a34ec54f90e80c0acef3d6334f762b8546
Eriberto Guzman <eriberto.guzman@outlook.com>
2021-07-16 12:05:56 -0400

    commit tree a16a55affa17298dd0489fdcf546fe19a2035e33

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def initialize(name = "" )
+    @name = name
+  end
 
   def self.all
     @@all
@@ -8,6 +14,24 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    # NOTHINGIN HERE SO ITS NILL
+    song = self.new
+    binding.pry
+    @@all << song# this returns an array, returns the new array
+    return song
+  end
+
+  def self.new_by_name(name)
+    self.new(name)
+  end
+
+  def self.create_by_name(name)
+    song = self.new(name)
+    @@all << song
+    return song #explicit
   end
 
 end
